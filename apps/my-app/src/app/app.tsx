a6df4a6c701adb3d5518cc9c844e1060b588b6c2
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2024-03-29 11:54:10 +0700

    :sparkles: My-app: Clear out stuffs

--- apps/my-app/src/app/app.tsx
+++ apps/my-app/src/app/app.tsx
@@ -1,12 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
     <div>
-      <NxWelcome title="my-app" />
+      <div>Hello world</div>
     </div>
   );
 }
