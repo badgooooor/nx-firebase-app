14749da7ca95279caeae62a48ca8f428c42aa4ae
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2024-03-31 14:29:15 +0700

    :sparkles: Scaffold my-app-2

---
+++ apps/my-app-2/src/app/app.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@acme/ui';
+
+export function App() {
+  return (
+    <div>
+      <div className="bg-indigo-500 p-2 font-mono">Hello world from app 2</div>
+      <Button>Shared button</Button>
+    </div>
+  );
+}
+
+export default App;
