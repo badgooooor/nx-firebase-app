6a5553335c902a0f77d9390b8175d0bac37d7ab7
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2024-03-31 22:41:57 +0700

    :test_tube: Fix test

--- apps/my-app-2/src/app/app.spec.tsx
+++ apps/my-app-2/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome my-app-2/gi)).toBeTruthy();
+    expect(getByText(/Hello world/gi)).toBeTruthy();
   });
 });
