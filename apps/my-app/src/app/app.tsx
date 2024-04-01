abcf46489689a9d6934a72db6fa6ba61cd0f5bd9
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2024-04-01 15:32:20 +0700

    :sparkles: Change title

--- apps/my-app/src/app/app.tsx
+++ apps/my-app/src/app/app.tsx
@@ -3,7 +3,7 @@ import { Button } from '@acme/ui';
 export function App() {
   return (
     <div>
-      <div className="bg-indigo-500 p-2 font-mono">Hello world</div>
+      <div className="bg-indigo-500 p-2 font-mono">Hello world from app-1</div>
       <Button>Shared button</Button>
     </div>
   );
